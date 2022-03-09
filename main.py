d444f90d7e1dca0cebdcc2ed90179139a84f19b8
Md. Mashur Shalehin Sakib <41992136+h0oni@users.noreply.github.com>
2022-03-09 17:17:46 +0600

    adding wafermap api

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 from fastapi import FastAPI
-import tensorflow as tf
-import tensorflow_hub as hub
 import numpy as np
-from tensorflow.keras import preprocessing
-import PIL
-import base64
-from io import BytesIO
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-
+import json
 
 app = FastAPI()
 
@@ -23,28 +17,14 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-new_model = tf.keras.models.load_model('best.h5',custom_objects={'KerasLayer':hub.KerasLayer})
+f = open('wafermap.json')
+
+wafermap = json.load(f)
 
 @app.get('/')
 async def hello():
     return {'hello':'world'}
 
-@app.post('/predict/')
-async def test(item: Post):
-    # im_b64 = item.img
-    # if not(im_b64):
-    #     return {'hello':'oops'}
-
-    #b64 -> pil
-    image = PIL.Image.open(BytesIO(base64.b64decode(item.img[22:])))
-    image.save('test.png')
-
-    img = preprocessing.image.load_img('test.png', target_size = (224, 224))
-    np_image = np.array(img).astype('float32')/255
-    np_image = np.expand_dims(np_image, axis=0)
-    del image, img
-    
-    proba = new_model.predict(np_image)
-    pred = np.argmax(proba)
-
-    return {'hello':f'{pred} ({(100*tf.reduce_max(proba)) : .2f}%)'}
+@app.post('/wafermap/')
+async def test():
+    return wafermap
